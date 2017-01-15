600a2866f5b52e7ce17466c6c379de3abbf9310e
Amna <amna.n.liaqat@gmail.com>
2017-01-14 23:52:52 -0500

    added adjustable panels

--- textbox.py
+++ textbox.py
@@ -2,34 +2,36 @@ import Tkinter as tk
 
 root = tk.Tk()
 
-h = 40
-w = 100
+###############################################################################
+##############Instructions#####################################################
+###############################################################################
 instructions = "Select and analyze a theme from 'Hamlet' by William Shakespeare. How does the use of literary devices support the theme you have selected?"
-# msg = Label(root, justify=LEFT, pady=10, text=instructions)
-def enter(event):
-    # replace with update checklist function
-    print("update checklist") 
-def bye(event):
-    print("bye")
-
-def checked(event, x):
-    print(x)
-    # replace with update article lists panel function
-    if x is 0:
-    	print('checked')
-    if x is 1:
-    	print('unchecked')
 
 msg = tk.Message(root, text = instructions)
 msg.config(font=('bold'), width=700, pady=10)
 msg.grid(row=0, column =1, rowspan=2)
 
+###############################################################################
+##############TEXTBOX##########################################################
+###############################################################################
+def enter(event):
+    # replace with update checklist function
+    print("update checklist") 
+
+h = 40
+w = 100
+
+textPanel = tk.PanedWindow(orient=tk.HORIZONTAL)
+textPanel.config(bg = 'black')
+textPanel.grid(row = 2, column = 1, rowspan = h)
+
 S = tk.Scrollbar(root)
-T = tk.Text(root, height=h, width=w, pady=10)
+T = tk.Text(textPanel, height=h, width=w, pady=10)
 # S.pack(side=RIGHT, fill=Y)
 # T.pack(side=LEFT, fill=Y)
 S.grid(row =2, column =2, rowspan=h)
-T.grid(row=2, column =1, rowspan=h)
+# T.grid(row=2, column =1, rowspan=h)
+textPanel.add(T)
 
 T.bind('<Return>', enter)
 
@@ -46,17 +48,32 @@ That flesh is heir to. 'Tis a consummation
 Devoutly to be wished."""
 T.insert(tk.END, quote)
 
+###############################################################################
+##############CHECKLIST########################################################
+###############################################################################
 terms = ['Hamlet', 'nobler', 'suffer', 'slings', 'arrows', 'fortunes']
-# master = Tk()
-# var1 = tk.IntVar()
-# check1 = tk.Checkbutton(root, text="Hamlet", variable=var1)
-# check1.grid(row=2, column =4, sticky=tk.W)
-# check1.bind('<Button-1>', hello)
+
+listPanel = tk.PanedWindow(orient=tk.VERTICAL)
+listPanel.config(bg = 'black')
+
+checklistFrame = tk.Frame(listPanel)
+
+# listPanel.grid(row = 2, column = 4, rowspan = h)
+textPanel.add(listPanel)
+listPanel.add(checklistFrame)
+def checked(event, x):
+    print(x)
+    # replace with update article lists panel function
+    if x is 0:
+    	print('checked')
+    if x is 1:
+    	print('unchecked')
 
 for term in terms:
 	var = tk.IntVar()
-	item = tk.Checkbutton(root, text=term, variable=var)
+	item = tk.Checkbutton(checklistFrame, text=term, variable=var)
 	x = var
+	# checklistFrame.add(item)
 	item.grid(row=terms.index(term) + 2, column =4, sticky=tk.W)
 
 	def checkedHelp(event, var=var): 
@@ -64,4 +81,42 @@ for term in terms:
 	
 	item.bind('<Button-1>', checkedHelp)
 
+###############################################################################
+##############READING RESOURCES################################################
+###############################################################################
+
+top = tk.Label(listPanel, text="Reading Resources")
+listPanel.add(top)
+
+bottom = tk.Label(listPanel, text="Article Preview")
+listPanel.add(bottom)
+
+
 tk.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
